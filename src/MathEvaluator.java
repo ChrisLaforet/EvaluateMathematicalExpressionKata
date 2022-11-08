81c37bc0b2d775294326a0141cd60943c0fab32b
christopher.laforet <christopher.laforet@silverrailtech.com>
2022-11-08 14:11:11 -0500

    initial steps in returning a simple expression of an integer or decimal

--- src/MathEvaluator.java
+++ src/MathEvaluator.java
@@ -1,7 +1,29 @@
 public class MathEvaluator {
 
   public double calculate(String expression) {
-  
+	  return new Value(expression).evaluate();
   }
 
+  
+  static abstract class Expression {
+	  public abstract double evaluate();
+  }
+  
+  static class Value extends Expression {
+	  private double value;
+	  
+	  public Value(String value) {
+		  this.value = Double.parseDouble(value);
+	  }
+	  
+	  public double evaluate() {
+		  return value;
+	  }
+	  
+  }
+  
+  static class Parser {
+	  
+  }
+  
 }
--- src/MathEvaluatorTest.java
+++ src/MathEvaluatorTest.java
@@ -3,53 +3,70 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class MathEvaluatorTest {
+
 	// Additional tests 
 	
+	@Test
+	public void givenExpressionOfInteger_whenValueIs1_thenReturnsValueAsDouble() {
+		assertEquals(1.0, new MathEvaluator().calculate("1"));
+	}
 	
+
+	@Test
+	public void givenExpressionOfInteger_whenValueIs2_thenReturnsValueAsDouble() {
+		assertEquals(2.0, new MathEvaluator().calculate("2"));
+	}
+	
+	@Test
+	public void givenExpressionOfDouble_whenValueIncludesDecimal_thenReturnsValueAsDouble() {
+		assertEquals(2.12345, new MathEvaluator().calculate("2.12345"));
+	}
+	
+
 	
 	// Original tests from CodeWars
-	@Test
-	public void testAddition() {
-		assertEquals(new MathEvaluator().calculate("1+1"), 2d, 0.01);
-	}
-
-	@Test
-	public void testSubtraction() {
-		assertEquals(new MathEvaluator().calculate("1 - 1"), 0d, 0.01);
-	}
-
-	@Test
-	public void testMultiplication() {
-		assertEquals(new MathEvaluator().calculate("1* 1"), 1d, 0.01);
-	}
-
-	@Test
-	public void testDivision() {
-		assertEquals(new MathEvaluator().calculate("1 /1"), 1d, 0.01);
-	}
-
-	@Test
-	public void testNegative() {
-		assertEquals(new MathEvaluator().calculate("-123"), -123d, 0.01);
-	}
-
-	@Test
-	public void testLiteral() {
-		assertEquals(new MathEvaluator().calculate("123"), 123d, 0.01);
-	}
-
-	@Test
-	public void testExpression() {
-		assertEquals(new MathEvaluator().calculate("2 /2+3 * 4.75- -6"), 21.25, 0.01);
-	}
-
-	@Test
-	public void testSimple() {
-		assertEquals(new MathEvaluator().calculate("12* 123"), 1476d, 0.01);
-	}
-
-	@Test
-	public void testComplex() {
-		assertEquals(new MathEvaluator().calculate("2 / (2 + 3) * 4.33 - -6"), 7.732, 0.01);
-	}
+//	@Test
+//	public void testAddition() {
+//		assertEquals(new MathEvaluator().calculate("1+1"), 2d, 0.01);
+//	}
+//
+//	@Test
+//	public void testSubtraction() {
+//		assertEquals(new MathEvaluator().calculate("1 - 1"), 0d, 0.01);
+//	}
+//
+//	@Test
+//	public void testMultiplication() {
+//		assertEquals(new MathEvaluator().calculate("1* 1"), 1d, 0.01);
+//	}
+//
+//	@Test
+//	public void testDivision() {
+//		assertEquals(new MathEvaluator().calculate("1 /1"), 1d, 0.01);
+//	}
+//
+//	@Test
+//	public void testNegative() {
+//		assertEquals(new MathEvaluator().calculate("-123"), -123d, 0.01);
+//	}
+//
+//	@Test
+//	public void testLiteral() {
+//		assertEquals(new MathEvaluator().calculate("123"), 123d, 0.01);
+//	}
+//
+//	@Test
+//	public void testExpression() {
+//		assertEquals(new MathEvaluator().calculate("2 /2+3 * 4.75- -6"), 21.25, 0.01);
+//	}
+//
+//	@Test
+//	public void testSimple() {
+//		assertEquals(new MathEvaluator().calculate("12* 123"), 1476d, 0.01);
+//	}
+//
+//	@Test
+//	public void testComplex() {
+//		assertEquals(new MathEvaluator().calculate("2 / (2 + 3) * 4.33 - -6"), 7.732, 0.01);
+//	}
 }
